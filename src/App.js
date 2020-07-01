fc5e3b9130c86c718cc90e738af51197599c8450
?ranky <sammysmart95@gmail.com>
2020-07-01 18:56:34 +0100

    created a demo movie app using imdb apikey

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import Search from "./components/Search";
+import Results from "./components/Results";
+import Popup from "./components/Popup";
 
-function App() {
+// console.log(process.env.REACT_APP_IMDB_API_KEY);
+
+const App = () => {
+  const key = process.env.REACT_APP_IMDB_API_KEY;
+  const apiurl = "http://www.omdbapi.com/";
+  const apikey = `&apikey=${key}`;
+
+  const [state, setState] = useState({
+    text: "",
+    results: [],
+    selected: {},
+  });
+
+  const handleInput = (e) => {
+    let text = e.target.value;
+    setState((prevState) => {
+      return { ...prevState, text: text };
+    });
+  };
+
+  const search = (e) => {
+    if (e.key === "Enter") {
+      axios(apiurl + "?s=" + state.text + apikey).then(({ data }) => {
+        let results = data.Search;
+        console.log(results);
+        setState((prevState) => {
+          return {
+            ...prevState,
+            results: results,
+          };
+        });
+      });
+    }
+  };
+
+  const openPopup = (id) => {
+    axios(apiurl + "?i=" + id + apikey).then(({ data }) => {
+      let result = data;
+      console.log(result);
+      setState((prevState) => {
+        return {
+          ...prevState,
+          selected: result,
+        };
+      });
+    });
+  };
+  const closePopup = () => {
+    setState((prevState) => {
+      return {
+        ...prevState,
+        selected: {},
+      };
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Movie Database App</h1>
       </header>
+      <main>
+        <Search handleInput={handleInput} search={search} />
+
+        <Results results={state.results} openPopup={openPopup} />
+
+        {typeof state.selected.Title != "undefined" ? (
+          <Popup selected={state.selected} closePopup={closePopup} />
+        ) : (
+          false
+        )}
+      </main>
     </div>
   );
-}
+};
 
 export default App;
